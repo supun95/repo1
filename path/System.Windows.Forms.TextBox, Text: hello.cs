1aa2d6804bf2be4191b57eadafdcb8dbc1626928
supun95 <supundissanayake8@gmail.com>
2017-01-04 11:28:29 +0000

    File creation

---
+++ path/System.Windows.Forms.TextBox, Text: hello.cs
@@ -0,0 +1,20 @@
+   public void member_bug_assign()
+        {
+            mySqlConnection.Open();
+            String select_user = "SELECT * FROM users";
+            SqlCommand SqlCommand = new SqlCommand(select_user, mySqlConnection);
+            try
+            {
+                SqlDataReader Data_reader = SqlCommand.ExecuteReader();
+
+                while (Data_reader.Read())
+                {
+                    assign_input.Items.Add(Data_reader["user_name"]);
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, System.Windows.Forms.Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            mySqlConnection.Close();
+        }
